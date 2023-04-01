f87516006b71c267c8a548382ae5d467824401e7
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-01 15:09:20 -0300

    Change url for more data

--- src/lib/getData.ts
+++ src/lib/getData.ts
@@ -3,7 +3,7 @@ import { currentWeatherStore } from './store';
 const getCurrentWeather = async (city = 'reconquista') => {
 	try {
 		const response = await fetch(
-			`https://api.weatherapi.com/v1/current.json?key=0a29cac888c648b39b7161817232903&q=${city}&days=3&aqi=no&alerts=yes`,
+			`https://api.weatherapi.com/v1/forecast.json?key=0a29cac888c648b39b7161817232903&q=${city}&days=3&aqi=no&alerts=no`,
 			{ mode: 'cors' }
 		);
 		const data = await response.json();

115b0f8f5e99a489335ec6a86cfbf04e3268bd4b
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-01 15:57:43 -0300

    Add html for rendering forecast

--- src/components/daily/DailyNormal.svelte
+++ src/components/daily/DailyNormal.svelte
@@ -10,11 +10,21 @@
 	console.log('daily', weather);
 </script>
 
-<div id="main">
-	<div id="card1">
-		<img src="" alt="" srcset="" />
+{#each weather as days, i}
+	<div id="main">
+		<div id="card{i}">
+			<p>{days.date}</p>
+			<div>
+				<img src={days.day.condition.icon} alt={days.day.condition.text} />
+				<p><strong>{days.day.condition.text}</strong></p>
+			</div>
+			<p><strong>Max Temperature: </strong>{days.day.maxtemp_c}°C</p>
+			<p><strong>Min Temperature: </strong>{days.day.mintemp_c}°C</p>
+			<p><strong>Humidity: </strong>{days.day.avghumidity}%</p>
+			<p><strong>Change of Rain: </strong>{days.day.daily_chance_of_rain}%</p>
+		</div>
 	</div>
-</div>
+{/each}
 
 <style lang="postcss">
 </style>

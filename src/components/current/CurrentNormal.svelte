486f48c59ca9c601c73eb7f3c86e61b48400a413
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-01 17:04:54 -0300

    change store and way of rendering

--- src/components/current/CurrentNormal.svelte
+++ src/components/current/CurrentNormal.svelte
@@ -1,36 +1,35 @@
 <script>
-	import { currentWeatherStore } from '../../lib/store';
-
-	let weather;
-
-	currentWeatherStore.subscribe((value) => {
-		weather = value;
-	});
+	import { completeWeather } from '../../lib/store';
 </script>
 
-<div class="main mx-auto flex h-80 gap-20">
-	<div class="flex flex-col items-center justify-around">
-		<img
-			class="h-60 object-fill"
-			src={weather.current.condition.icon}
-			alt="condition imagen {weather.current.condition.text}"
-		/>
-		<p class="text-xl"><strong>{weather.current.condition.text}</strong></p>
+{#if $completeWeather}
+	<div class="main mx-auto flex h-80 gap-20">
+		<div class="flex flex-col items-center justify-around">
+			<img
+				class="h-60 object-fill"
+				src={$completeWeather.current.condition.icon}
+				alt="condition imagen {$completeWeather.current.condition.text}"
+			/>
+			<p class="text-xl"><strong>{$completeWeather.current.condition.text}</strong></p>
+		</div>
+		<div class="flex flex-col justify-evenly">
+			<p class="text-xl"><strong>Temperature: </strong>{$completeWeather.current.temp_c} °C</p>
+			<p class="text-xl"><strong>Feels Like: </strong>{$completeWeather.current.feelslike_c} °C</p>
+			<p class="text-xl"><strong>Humidity: </strong>{$completeWeather.current.humidity}%</p>
+			<p class="text-xl"><strong>Wind Speed: </strong>{$completeWeather.current.wind_kph} Kph</p>
+			<p class="text-xl"><strong>Wind direction: </strong>{$completeWeather.current.wind_dir}</p>
+			<p class="text-xl"><strong>Pressure: </strong>{$completeWeather.current.pressure_mb} mbar</p>
+			<br />
+			<p class="text-base">
+				{$completeWeather.location.name}, {$completeWeather.location.region}, {$completeWeather
+					.location.country}
+			</p>
+			<p class="text-base"><strong>Last Update: </strong>{$completeWeather.current.last_updated}</p>
+		</div>
 	</div>
-	<div class="flex flex-col justify-evenly">
-		<p class="text-xl"><strong>Temperature: </strong>{weather.current.temp_c} °C</p>
-		<p class="text-xl"><strong>Feels Like: </strong>{weather.current.feelslike_c} °C</p>
-		<p class="text-xl"><strong>Humidity: </strong>{weather.current.humidity}%</p>
-		<p class="text-xl"><strong>Wind Speed: </strong>{weather.current.wind_kph} Kph</p>
-		<p class="text-xl"><strong>Wind direction: </strong>{weather.current.wind_dir}</p>
-		<p class="text-xl"><strong>Pressure: </strong>{weather.current.pressure_mb} mbar</p>
-		<br />
-		<p class="text-base">
-			{weather.location.name}, {weather.location.region}, {weather.location.country}
-		</p>
-		<p class="text-base"><strong>Last Update: </strong>{weather.current.last_updated}</p>
-	</div>
-</div>
+{:else}
+	<p>Loading Data</p>
+{/if}
 
 <style lang="postcss">
 	div.main {

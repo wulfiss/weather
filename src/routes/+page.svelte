d06918bfb410db7390234f5d7f5bbaf250312b1a
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-03 16:12:17 -0300

    Update imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import CurrentNormal from '../components/current/CurrentNormal.svelte';
-	import DailyNormal from '../components/daily/DailyNormal.svelte';
+	import Daily from '../components/daily/Daily.svelte';
 	import Hourly from '../components/hourly/Hourly.svelte';
 	import { onMount } from 'svelte';
 	import { getCurrentWeather } from '../lib/getData';

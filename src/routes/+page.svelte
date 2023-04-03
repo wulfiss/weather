da4af6bb6fd267c8557c471acbd7039dfbb0bdf0
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-03 18:17:10 -0300

    Get the btn hourly&daily working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,33 @@
 <script lang="ts">
 	import CurrentNormal from '../components/current/CurrentNormal.svelte';
-	import Daily from '../components/daily/Daily.svelte';
-	import Hourly from '../components/hourly/hourly.svelte';
+	import DailyMain from '../components/daily/DailyMain.svelte';
+	import HourlyMain from '../components/hourly/HourlyMain.svelte';
 	import { onMount } from 'svelte';
 	import { getCurrentWeather } from '../lib/getData';
-
-	let tabs = [
-		{ id: 0, a: 'Daily', name: 'Calculadora Rápida' },
-		{ id: 1, a: 'Hourly', name: 'Calculadora' }
-	];
-
-	const actions = [Daily, Hourly];
-
-	let action = actions[0];
-
-	function handleActionClicked(id: number): any {
-		console.log('A', action);
-		action = actions[id];
-		console.log('b', action);
-	}
 
 	onMount(() => {
 		getCurrentWeather();
 	});
 
 	let current = 'daily';
+	let action = DailyMain;
 </script>
 
 <svelte:component this={CurrentNormal} />
+
 <div class="btn-group flex h-16 w-full items-center justify-center">
 	<button
 		class={current === 'daily' ? 'selected' : 'unSelected'}
-		on:click={handleActionClicked(0)}
-		on:click={() => (current = 'daily')}>Daily</button
-	>
+		on:click={() => (action = DailyMain)}
+		on:click={() => (current = 'daily')}
+		>Daily
+	</button>
 	<button
 		class={current === 'hourly' ? 'selected' : 'unSelected'}
-		on:click={handleActionClicked(1)}
-		on:click={() => (current = 'hourly')}>Hourly</button
-	>
+		on:click={() => (action = HourlyMain)}
+		on:click={() => (current = 'hourly')}
+		>Hourly
+	</button>
 </div>
 
 <svelte:component this={action} />

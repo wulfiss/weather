ca87d01348b912baabff8690e9396273388aa5d1
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-03 16:53:18 -0300

    Add btn for change hourly to daily

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,23 @@
 	onMount(() => {
 		getCurrentWeather();
 	});
+
+	let current = 'daily';
 </script>
 
 <svelte:component this={CurrentNormal} />
+<div class="btn-group flex h-16 w-full items-center justify-center">
+	<button class={current === 'daily' ? 'selected' : 'unSelected'} on:click={() => (current = 'daily')}>Daily</button>
+	<button class={current === 'hourly' ? 'selected' : 'unSelected'} on:click={() => (current = 'hourly')}>Hourly</button>
+</div>
 <!-- <svelte:component this={DailyNormal} /> -->
 <svelte:component this={Hourly} />
+
+<style lang="postcss">
+	.selected {
+		@apply btn-active btn h-7 w-20;
+	}
+	.unSelected {
+		@apply btn h-7 w-20;
+	}
+</style>

da3182008fcbdeaa3b507125e465ad89f78f4c08
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-06 19:31:28 -0300

    Add styling for not valid in search

--- src/components/SearchBar/SearchBar.svelte
+++ src/components/SearchBar/SearchBar.svelte
@@ -3,25 +3,38 @@
 	import { setLocaStorage, validateInput } from '$lib/util';
 
 	let cityName: string;
+	let valid: boolean = true;
 
 	const handleClick = () => {
 		if (!validateInput(cityName)) {
-			console.error('Enter a city');
+			valid = false;
+			cityName = '';
 			return false;
 		}
 		getCurrentWeather(cityName);
 		setLocaStorage(cityName, 'location');
 		cityName = '';
+		valid = true;
 	};
 </script>
 
 <form id="findCity" action="" class="flex h-20 w-full flex-row items-center justify-center gap-1 sm:h-full sm:gap-2">
-	<input
-		type="text"
-		placeholder="Enter a city"
-		class="input-bordered input input-sm h-10 w-64 max-w-xs sm:w-full"
-		bind:value={cityName}
-	/>
+	{#if valid}
+		<input
+			type="text"
+			placeholder="Enter a city and country"
+			class="input-bordered input input-sm h-10 w-64 max-w-xs sm:w-full"
+			bind:value={cityName}
+		/>
+	{:else}
+		<input
+			type="text"
+			placeholder="ENTER A VALID CITY NAME"
+			class="input-bordered input input-sm h-10 w-64 max-w-xs border-red-700 placeholder:font-semibold placeholder:text-red-700 sm:w-full"
+			bind:value={cityName}
+		/>
+	{/if}
+
 	<button on:click={handleClick} class="md:btn-auto btn-sm btn mx-2 h-10 w-20 px-3">Search</button>
 </form>
 

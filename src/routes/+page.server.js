5532bdad0557630348a54cf5d4839ae62d21edd8
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-08 02:15:51 -0300

    Try to hide the key again

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,8 @@
-import { API_KEY } from '$env/static/private';
+import { VERCEL_API_KEY } from '$env/static/private';
 
 /** @type {import('./$types').LayoutServerLoad} */
 export function load() {
 	return {
-		API_KEY: API_KEY
+		API_KEY: VERCEL_API_KEY
 	};
 }

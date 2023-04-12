8fcaf4134f399b50cb59448f4a7006f72660b960
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-12 19:59:53 -0300

    add documentation

--- src/routes/api/weather/+server.ts
+++ src/routes/api/weather/+server.ts
@@ -24,25 +24,37 @@ export const GET: RequestHandler = async ({ url }) => {
 	return new Response(JSON.stringify(data));
 };
 
-/* export async function GET({ query }: { query: URLSearchParams }) {
-	console.log(query);
-	const city = query.get('city');
-	console.log(city);
+/* ```js
+// This module defines a GET handler for the /api/weather endpoint
+// It uses the weatherapi.com service to fetch weather data for a given city
+// It requires an environment variable VERCEL_API_KEY that contains the API key for weatherapi.com
 
-	if (!city) {
-		throw error(400, 'Missing city parameter');
-	}
+import { error } from '@sveltejs/kit'; // import the error helper from SvelteKit
+import type { RequestHandler } from './$types'; // import the RequestHandler type from SvelteKit
+import dotenv from 'dotenv'; // import the dotenv module to load environment variables from .env file
+dotenv.config(); // load the .env file
 
-	const API_KEY = '0a29cac888c648b39b7161817232903';
-	const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=no&alerts=no`;
+export const GET: RequestHandler = async ({ url }) => {
+  // define a GET handler that takes a url parameter and returns a Response object
+  const city = url.searchParams.get('city'); // get the city parameter from the query string
 
-	const response = await fetch(API_URL, { mode: 'cors' });
+  if (!city) {
+    // check if city is valid
+    throw error(400, 'Missing city parameter'); // throw an error with status code 400 and message 'Missing city parameter'
+  }
 
-	if (!response.ok) {
-		throw error(response.status, response.statusText);
-	}
+  const API_KEY = process.env.VERCEL_API_KEY; // get the API key from environment variable
+  const API_URL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=no&alerts=no`; // construct the API URL with the API key and city
 
-	const data = await response.json();
+  const response = await fetch(API_URL, { mode: 'cors' }); // fetch the data from the API with cors mode
 
-	return new Response(JSON.stringify(data));
-} */
+  if (!response.ok) {
+    // check if the response is ok
+    throw error(response.status, response.statusText); // throw an error with the response status code and status text
+  }
+
+  const data = await response.json(); // parse the JSON data
+
+  return new Response(JSON.stringify(data)); // return the data as JSON
+};
+``` */

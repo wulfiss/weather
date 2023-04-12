9af833de89fe837003475247009cc49f41b474a7
Villalba Julian <96155980+wulfiss@users.noreply.github.com>
2023-04-12 20:36:03 -0300

    Remove test code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,6 @@
 	export let data;
 
 	let key = data.API_KEY;
-	import { onMount } from 'svelte';
-	let weather;
-
-	onMount(async () => {
-		const response = await fetch('/api/weather?city=London');
-		weather = await response.json();
-	});
 </script>
 
 <header class="w-full flex-1 justify-center sm:mx-1 sm:h-20 sm:w-auto">
@@ -22,21 +15,6 @@
 	<slot />
 </main>
 <Footer />
-
-{#if weather}
-	<h1>Weather forecast for {weather.location.name}</h1>
-	<ul>
-		{#each weather.forecast.forecastday as day}
-			<li>
-				<img src={day.day.condition.icon} alt={day.day.condition.text} />
-				<span>{day.date}</span>
-				<span>{day.day.maxtemp_c}°C / {day.day.mintemp_c}°C</span>
-			</li>
-		{/each}
-	</ul>
-{:else}
-	<p>Loading...</p>
-{/if}
 
 <style lang="postcss">
 	* {

1ed1119aeb19283826936a78f794569367a5e065
Julian Wulff <96155980+wulfiss@users.noreply.github.com>
2023-04-12 20:02:18 -0300

    it's a test for the endpoint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,17 @@
 	import '../app.css';
 	import SearchBar from '../components/SearchBar/SearchBar.svelte';
 	import Footer from '../components/Footer/Footer.svelte';
+
 	export let data;
 
 	let key = data.API_KEY;
+	import { onMount } from 'svelte';
+	let weather;
+
+	onMount(async () => {
+		const response = await fetch('/api/weather?city=London');
+		weather = await response.json();
+	});
 </script>
 
 <header class="w-full flex-1 justify-center sm:mx-1 sm:h-20 sm:w-auto">
@@ -15,6 +23,20 @@
 </main>
 <Footer />
 
+{#if weather}
+	<h1>Weather forecast for {weather.location.name}</h1>
+	<ul>
+		{#each weather.forecast.forecastday as day}
+			<li>
+				<img src={day.day.condition.icon} alt={day.day.condition.text} />
+				<span>{day.date}</span>
+				<span>{day.day.maxtemp_c}°C / {day.day.mintemp_c}°C</span>
+			</li>
+		{/each}
+	</ul>
+{:else}
+	<p>Loading...</p>
+{/if}
 
 <style lang="postcss">
 	* {

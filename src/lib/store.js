c94519de4e481ff367dc8f1075e5f247260a81eb
Villalba Julian <96155980+wulfiss@users.noreply.github.com>
2023-04-18 19:43:11 -0300

    Add store for card

--- src/lib/store.js
+++ src/lib/store.js
@@ -3,5 +3,6 @@ import { writable } from 'svelte/store';
 const completeWeather = writable();
 const units = writable({ unit: 'metric' });
 const times = writable({ time: '24hours' });
+const validity = writable('true');
 
-export { completeWeather, units, times };
+export { completeWeather, units, times, validity };

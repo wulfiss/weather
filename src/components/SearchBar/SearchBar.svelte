27e2c43358e8587a983f008dfc0bc5ed144b5540
Villalba Julian <96155980+wulfiss@users.noreply.github.com>
2023-04-18 20:07:51 -0300

    clean the code

--- src/components/SearchBar/SearchBar.svelte
+++ src/components/SearchBar/SearchBar.svelte
@@ -14,32 +14,19 @@
 		}
 
 		getData(cityName);
-
-		if ($validity === 'true') {
-			setLocalStorage(cityName, 'location');
-		}
+		setLocalStorage(cityName, 'location');
 		cityName = '';
 		valid = true;
 	};
 </script>
 
 <form id="findCity" action="" class="flex h-16 w-full flex-row items-center justify-center gap-1 sm:h-full sm:gap-2">
-	{#if $validity === 'true'}
-		<input
-			type="text"
-			placeholder="Enter a city and country"
-			class="input-bordered input input-sm h-10 w-64 max-w-xs sm:w-full"
-			bind:value={cityName}
-		/>
-	{:else if $validity === 'invalid'}
-		<input
-			type="text"
-			placeholder="ENTER A VALID CITY NAME"
-			class="input-bordered input input-sm h-10 w-64 max-w-xs border-red-700 placeholder:font-semibold placeholder:text-red-700 sm:w-full"
-			bind:value={cityName}
-		/>
-	{/if}
-
+	<input
+		type="text"
+		placeholder="Enter a city and country"
+		class="input-bordered input input-sm h-10 w-64 max-w-xs sm:w-full"
+		bind:value={cityName}
+	/>
 	<button on:click={handleClick} class="md:btn-auto btn-sm btn mx-2 h-10 w-20 px-3">Search</button>
 </form>
 

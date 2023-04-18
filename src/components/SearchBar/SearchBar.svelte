dd2c0e08cea7990c3aad2d3f5fa9f20246d4b3f8
Villalba Julian <96155980+wulfiss@users.noreply.github.com>
2023-04-18 19:45:24 -0300

    Modify it for trigger alert card

--- src/components/SearchBar/SearchBar.svelte
+++ src/components/SearchBar/SearchBar.svelte
@@ -1,31 +1,33 @@
 <script lang="ts">
 	import { setLocalStorage, validateInput, getData } from '$lib/util';
+	import { validity } from '$lib/store';
 
-	let cityName: string = '';
+	let cityName: string;
 	let valid: boolean = true;
 
 	const handleClick = () => {
-		if (!validateInput(cityName.trimEnd())) {
+		if (!validateInput(cityName)) {
 			valid = false;
+			validity.set('invalid');
 			cityName = '';
 			return false;
 		}
 		getData(cityName);
-		setLocalStorage(cityName.trimEnd(), 'location');
+		setLocalStorage(cityName, 'location');
 		cityName = '';
 		valid = true;
 	};
 </script>
 
 <form id="findCity" action="" class="flex h-16 w-full flex-row items-center justify-center gap-1 sm:h-full sm:gap-2">
-	{#if valid}
+	{#if $validity === 'true'}
 		<input
 			type="text"
 			placeholder="Enter a city and country"
 			class="input-bordered input input-sm h-10 w-64 max-w-xs sm:w-full"
 			bind:value={cityName}
 		/>
-	{:else}
+	{:else if $validity === 'invalid'}
 		<input
 			type="text"
 			placeholder="ENTER A VALID CITY NAME"

5bd1fed1abe937858bd2a3a7f411593d63f4302d
Villalba Julian <96155980+wulfiss@users.noreply.github.com>
2023-04-18 19:42:15 -0300

    Add alert card

---
+++ src/components/AlertCard/AlertCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { validity } from '$lib/store';
+	let trigger = false;
+
+	const handleClick = () => {
+		validity.set('true');
+		trigger = false;
+	};
+
+	$: if ($validity === 'invalid') {
+		trigger = true;
+	}
+</script>
+
+{#if trigger === true}
+	<div class="card absolute z-10 w-96 bg-neutral text-neutral-content">
+		<div class="card-body items-center text-center">
+			<h2 class="card-title">Error!</h2>
+			{#if $validity === 'invalid'}
+				<p>City name no valid.</p>
+				<p>Please enter valid one.</p>
+			{/if}
+			<div class="card-actions justify-end">
+				<button class="btn-primary btn" on:click={handleClick}>Accept</button>
+			</div>
+		</div>
+	</div>
+{/if}
